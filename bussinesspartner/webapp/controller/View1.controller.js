38a71db4f9b6c7a5b1870eed957514f782ebbdad
Zaved Ali <zavedalimeer@gmail.com>
2022-04-18 15:13:10 +0000

    First Commit

--- bussinesspartner/webapp/controller/View1.controller.js
+++ bussinesspartner/webapp/controller/View1.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Sorter",
+    "sap/ui/core/format/DateFormat",
+	"sap/m/ToolbarSpacer",
+	"sap/ui/table/library",
+	"sap/ui/thirdparty/jquery"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel,Sorter,DateFormat,ToolbarSpacer,library,jquery) {
         "use strict";
+        var SortOrder = library.SortOrder;
 
         return Controller.extend("bp.bussinesspartner.controller.View1", {
             onInit: function () {
-
+                
             }
+            
         });
     });
